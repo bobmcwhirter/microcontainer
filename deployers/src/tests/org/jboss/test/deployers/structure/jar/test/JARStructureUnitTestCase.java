a17d2cd2e72ce204963dce88033f3ceb0b014b65
adrian@jboss.org <adrian@jboss.org@84be2c1e-ba19-0410-b317-a758671a6fc1>
2006-09-16 13:12:22 +0000

    Fix the deployer testsuite now that directory urls contain a / on the end.
    git-svn-id: http://anonsvn.jboss.org/repos/jbossas/projects/microcontainer/trunk@56913 84be2c1e-ba19-0410-b317-a758671a6fc1

--- deployers/src/tests/org/jboss/test/deployers/structure/jar/test/JARStructureUnitTestCase.java
+++ deployers/src/tests/org/jboss/test/deployers/structure/jar/test/JARStructureUnitTestCase.java
@@ -144,7 +144,7 @@ public class JARStructureUnitTestCase extends BaseDeployersTest
       
       // Test it got all the candidates
       Map<String, Boolean> expected = new HashMap<String, Boolean>();
-      expected.put(getURL("/structure/jar/subdirnotajar/sub"), false);
+      expected.put(getURL("/structure/jar/subdirnotajar/sub/"), false);
       assertContexts(expected, context.getChildren());
       
       assertCandidatesNotValid(context);
@@ -156,7 +156,7 @@ public class JARStructureUnitTestCase extends BaseDeployersTest
       
       // Test it got all the candidates
       Map<String, Boolean> expected = new HashMap<String, Boolean>();
-      expected.put(getURL("/structure/jar/subdirisajar/sub.jar"), true);
+      expected.put(getURL("/structure/jar/subdirisajar/sub.jar/"), true);
       assertContexts(expected, context.getChildren());
       
       assertCandidatesValid(context);
@@ -173,7 +173,7 @@ public class JARStructureUnitTestCase extends BaseDeployersTest
       
       // Test it got all the candidates
       Map<String, Boolean> expected = new HashMap<String, Boolean>();
-      expected.put(getURL("/structure/jar/subdirhasmetainf/sub"), true);
+      expected.put(getURL("/structure/jar/subdirhasmetainf/sub/"), true);
       assertContexts(expected, context.getChildren());
       
       assertCandidatesValid(context);
