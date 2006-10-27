7e06924b7165130a08e9cb0187ac3d47111d2c3a
alesj <alesj@84be2c1e-ba19-0410-b317-a758671a6fc1>
2006-10-27 13:55:06 +0000

    forgot retention, target annotations
    git-svn-id: http://anonsvn.jboss.org/repos/jbossas/projects/microcontainer/trunk@57894 84be2c1e-ba19-0410-b317-a758671a6fc1

--- kernel/src/main/org/jboss/beans/metadata/spi/annotations/StartLifecycle.java
+++ kernel/src/main/org/jboss/beans/metadata/spi/annotations/StartLifecycle.java
@@ -1,8 +1,15 @@
 package org.jboss.beans.metadata.spi.annotations;
 
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import java.lang.annotation.ElementType;
+
 /**
  * @author <a href="mailto:ales.justin@gmail.com">Ales Justin</a>
  */
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.METHOD})
 public @interface StartLifecycle
 {
 }
