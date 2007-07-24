9eb63ea8de5a26e83f944341e18ef24bce226ebf
adrian@jboss.org <adrian@jboss.org@84be2c1e-ba19-0410-b317-a758671a6fc1>
2007-07-24 16:57:18 +0000

    Add an easy way for the package class filter to include standard java classes as well.
    git-svn-id: http://anonsvn.jboss.org/repos/jbossas/projects/microcontainer/trunk@64253 84be2c1e-ba19-0410-b317-a758671a6fc1

--- classloader/src/main/org/jboss/classloader/spi/filter/PackageClassFilter.java
+++ classloader/src/main/org/jboss/classloader/spi/filter/PackageClassFilter.java
@@ -23,6 +23,7 @@ package org.jboss.classloader.spi.filter;
 
 import java.util.Arrays;
 
+import org.jboss.classloader.plugins.filter.JavaOnlyClassFilter;
 import org.jboss.classloader.plugins.filter.PatternClassFilter;
 
 /**
@@ -35,6 +36,9 @@ public class PackageClassFilter extends PatternClassFilter
 {
    /** The patterns as regular expressions */
    private String[] packageNames;
+   
+   /** Whether to include java */
+   private boolean includeJava = false;
    
    /**
     * Convert package names to class patterns
@@ -114,9 +118,51 @@ public class PackageClassFilter extends PatternClassFilter
       this.packageNames = packageNames;
    }
 
+   /**
+    * Get the includeJava.
+    * 
+    * @return the includeJava.
+    */
+   public boolean isIncludeJava()
+   {
+      return includeJava;
+   }
+
+   /**
+    * Set the includeJava.
+    * 
+    * @param includeJava the includeJava.
+    */
+   public void setIncludeJava(boolean includeJava)
+   {
+      this.includeJava = includeJava;
+   }
+
+   public boolean matchesClassName(String className)
+   {
+      if (super.matchesClassName(className))
+         return true;
+      if (includeJava == false)
+         return false;
+      return JavaOnlyClassFilter.INSTANCE.matchesClassName(className);
+   }
+
+   public boolean matchesResourcePath(String resourcePath)
+   {
+      if (super.matchesResourcePath(resourcePath))
+         return true;
+      if (includeJava == false)
+         return false;
+      return JavaOnlyClassFilter.INSTANCE.matchesResourcePath(resourcePath);
+   }
+
    @Override
    public String toString()
    {
-      return Arrays.asList(packageNames).toString();
+      StringBuilder builder = new StringBuilder();
+      builder.append(Arrays.asList(packageNames));
+      if (includeJava)
+         builder.append(" <INCLUDE_JAVA>");
+      return builder.toString();
    }
 }
