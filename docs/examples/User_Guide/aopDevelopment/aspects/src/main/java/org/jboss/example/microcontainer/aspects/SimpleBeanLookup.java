b40d8d373a95534d21b28d455dcc8e9cb5be5de1
newtonm <newtonm@84be2c1e-ba19-0410-b317-a758671a6fc1>
2007-11-09 09:49:09 +0000

    Migrated examples from ant to Maven2.
    git-svn-id: http://anonsvn.jboss.org/repos/jbossas/projects/microcontainer/trunk@66902 84be2c1e-ba19-0410-b317-a758671a6fc1

---
+++ docs/examples/User_Guide/aopDevelopment/aspects/src/main/java/org/jboss/example/microcontainer/aspects/SimpleBeanLookup.java
@@ -0,0 +1,52 @@
+/*
+* JBoss, Home of Professional Open Source
+* Copyright 2006, JBoss Inc., and individual contributors as indicated
+* by the @authors tag. See the copyright.txt in the distribution for a
+* full listing of individual contributors.
+*
+* This is free software; you can redistribute it and/or modify it
+* under the terms of the GNU Lesser General Public License as
+* published by the Free Software Foundation; either version 2.1 of
+* the License, or (at your option) any later version.
+*
+* This software is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+* Lesser General Public License for more details.
+*
+* You should have received a copy of the GNU Lesser General Public
+* License along with this software; if not, write to the Free
+* Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+* 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+*/
+package org.jboss.example.microcontainer.aspects;
+
+import java.util.Properties;
+import javax.naming.Context;
+import javax.naming.InitialContext;
+
+/**
+ * @author <a href="mailto:ales.justin@jboss.com">Ales Justin</a>
+ */
+public class SimpleBeanLookup
+{
+   private Properties env;
+
+   public String getName()
+   {
+      return getClass().getName();
+   }
+
+   public void setEnv(Properties env)
+   {
+      this.env = env;
+   }
+
+   public void start() throws Exception
+   {
+      Context context = new InitialContext(env);
+      System.out.println(context.lookup("beans/SimpleBean"));
+      System.out.println(context.lookup("beans/AnnotatedSimpleBean"));
+      System.out.println(context.lookup("beans/XmlAnnotatedSimpleBean"));
+   }
+}
--- docs/examples/spring/readme.txt
+++
@@ -1,3 +0,0 @@
-This example needs jboss-spring-int artifact in the classpath on order to run.
-We also need the latest change to JBossEntityResolver in jboss-common project
-and SingletonSchemaResolverFactory from JBossXB referencing MC-Spring schema.
