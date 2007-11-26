f837775587a816bcc090990cac7f016bdbf6a9e5
kabir.khan@jboss.com <kabir.khan@jboss.com@84be2c1e-ba19-0410-b317-a758671a6fc1>
2007-11-26 22:26:31 +0000

    Disable some commented out code pending the aop release
    git-svn-id: http://anonsvn.jboss.org/repos/jbossas/projects/microcontainer/trunk@67463 84be2c1e-ba19-0410-b317-a758671a6fc1

--- aop-mc-int/src/tests/org/jboss/test/microcontainer/beans/test/DomainSimpleTest.java
+++ aop-mc-int/src/tests/org/jboss/test/microcontainer/beans/test/DomainSimpleTest.java
@@ -2,7 +2,7 @@
 * JBoss, Home of Professional Open Source.
 * Copyright 2006, Red Hat Middleware LLC, and individual contributors
 * as indicated by the @author tags. See the copyright.txt file in the
-* distribution for a full listing of individual contributors. 
+* distribution for a full listing of individual contributors.
 *
 * This is free software; you can redistribute it and/or modify it
 * under the terms of the GNU Lesser General Public License as
@@ -18,7 +18,7 @@
 * License along with this software; if not, write to the Free
 * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
 * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
-*/ 
+*/
 package org.jboss.test.microcontainer.beans.test;
 
 import java.util.Collection;
@@ -33,7 +33,7 @@ import org.jboss.test.microcontainer.beans.TestInterceptor;
 
 
 /**
- * 
+ *
  * @author <a href="kabir.khan@jboss.com">Kabir Khan</a>
  * @version $Revision: 1.1 $
  */
@@ -56,7 +56,7 @@ public abstract class DomainSimpleTest extends DomainProxyTest
       assertEquals("This is only a test", TestAspectWithProperty.last.getSomeProperty());
       assertTrue(TestInterceptor.invoked);
       assertTrue(TestAspect.invoked);
-    
+
       AspectManager domain = getDomain("AOPDomain");
       checkArtifacts(domain, true);
    }
@@ -72,61 +72,56 @@ public abstract class DomainSimpleTest extends DomainProxyTest
       assertNull(TestAspectWithProperty.last);
       assertFalse(TestInterceptor.invoked);
       assertFalse(TestAspect.invoked);
-      
+
       AspectManager manager = getMainAspectManager();
       checkArtifacts(manager, false);
    }
-   
+
    private void checkArtifacts(AspectManager manager, boolean shouldBeThere)
    {
       Object o = manager.getTypedef("TypeDef");
       checkShouldBeThere(o, shouldBeThere);
-      
+
       o = manager.getCFlowStack("CFlow");
       checkShouldBeThere(o, shouldBeThere);
-      
+
       o = manager.getDynamicCFlow("DynamicCFlow");
       checkShouldBeThere(o, shouldBeThere);
-      
+
       o = manager.getPointcut("Prepare");
       checkShouldBeThere(o, shouldBeThere);
-      
+
       o = manager.getPointcut("NamedPointcut");
       checkShouldBeThere(o, shouldBeThere);
-   
+
       o = manager.getInterfaceIntroduction("MixinBinding");
       checkShouldBeThere(o, shouldBeThere);
-      
+
       List coll =  manager.getAnnotationIntroductions();
       checkShouldBeThere(coll, shouldBeThere);
-      
+
       coll = manager.getAnnotationOverrides();
       checkShouldBeThere(coll, shouldBeThere);
-      
+
       Map map = manager.getPrecedenceDefs();
       checkShouldBeThere(map, shouldBeThere);
-      
-      map = manager.getArrayReplacements();
-      checkShouldBeThere(map, shouldBeThere);
-      
-      try
-      {
-         o = manager.getArrayBinding("TestArrayBinding");
-         checkShouldBeThere(o, shouldBeThere);
-      }
-      catch (ClassCastException expected)
-      {
-         // TODO remove after AOP 2.0.0.CR1 which fixes the bug
-      }
-      
+
+      fail("enable following aop release");
+      //map = manager.getArrayReplacements();
+      //checkShouldBeThere(map, shouldBeThere);
+
+      fail("enable following aop release");
+      //o = manager.getArrayBinding("TestArrayBinding");
+      //checkShouldBeThere(o, shouldBeThere);
+
    }
-   
+
    private void checkShouldBeThere(Object o, boolean shouldBeThere)
    {
       if (shouldBeThere) assertNotNull(o);
       else assertNull(o);
    }
-   
+
    private void checkShouldBeThere(Collection coll, boolean shouldBeThere)
    {
       if (shouldBeThere)
@@ -142,7 +137,7 @@ public abstract class DomainSimpleTest extends DomainProxyTest
          }
       }
    }
-   
+
    private void checkShouldBeThere(Map coll, boolean shouldBeThere)
    {
       if (shouldBeThere)
