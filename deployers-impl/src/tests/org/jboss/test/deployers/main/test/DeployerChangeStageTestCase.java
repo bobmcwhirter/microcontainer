0684b40de2a1410e2772eac71412bf4b70f7f565
adrian@jboss.org <adrian@jboss.org@84be2c1e-ba19-0410-b317-a758671a6fc1>
2008-02-26 14:44:52 +0000

    Source code tidyup
    git-svn-id: http://anonsvn.jboss.org/repos/jbossas/projects/microcontainer/trunk@70101 84be2c1e-ba19-0410-b317-a758671a6fc1

--- deployers-impl/src/tests/org/jboss/test/deployers/main/test/DeployerChangeStageTestCase.java
+++ deployers-impl/src/tests/org/jboss/test/deployers/main/test/DeployerChangeStageTestCase.java
@@ -22,37 +22,15 @@
 package org.jboss.test.deployers.main.test;
 
 import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Random;
-import java.util.Set;
 
 import junit.framework.Test;
 
 import org.jboss.deployers.client.spi.DeployerClient;
 import org.jboss.deployers.client.spi.Deployment;
-import org.jboss.deployers.client.spi.main.MainDeployer;
-import org.jboss.deployers.plugins.main.MainDeployerImpl;
 import org.jboss.deployers.spi.DeploymentException;
-import org.jboss.deployers.spi.deployer.DeploymentStage;
 import org.jboss.deployers.spi.deployer.DeploymentStages;
-import org.jboss.deployers.spi.structure.StructureMetaDataFactory;
-import org.jboss.deployers.structure.spi.DeploymentContext;
 import org.jboss.deployers.structure.spi.DeploymentUnit;
-import org.jboss.deployers.structure.spi.StructuralDeployers;
-import org.jboss.deployers.structure.spi.StructureBuilder;
-import org.jboss.deployers.structure.spi.helpers.AbstractStructureBuilder;
-import org.jboss.test.deployers.main.support.AddDeploymentRunnable;
-import org.jboss.test.deployers.main.support.AddProcessRemoveProcessRunnable;
-import org.jboss.test.deployers.main.support.DeployRunnable;
-import org.jboss.test.deployers.main.support.DeployUndeployRunnable;
-import org.jboss.test.deployers.main.support.DeployerTestRunnable;
-import org.jboss.test.deployers.main.support.FailedDeployUndeployRunnable;
-import org.jboss.test.deployers.main.support.ProcessRunnable;
-import org.jboss.test.deployers.main.support.ShutdownRunnable;
-import org.jboss.test.deployers.main.support.TestDeployment;
-import org.jboss.test.deployers.main.support.UndeployRunnable;
 
 /**
  * DeployerChangeStageTestCase.
