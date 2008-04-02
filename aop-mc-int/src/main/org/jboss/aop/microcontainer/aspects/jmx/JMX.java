5f8fdeb5f2eafa9415fef6162e63241b170bc050
kabir.khan@jboss.com <kabir.khan@jboss.com@84be2c1e-ba19-0410-b317-a758671a6fc1>
2008-04-02 10:41:02 +0000

    Revert change that breaks JMXDecoratedTestCase
    git-svn-id: http://anonsvn.jboss.org/repos/jbossas/projects/microcontainer/trunk@71588 84be2c1e-ba19-0410-b317-a758671a6fc1

--- aop-mc-int/src/main/org/jboss/aop/microcontainer/aspects/jmx/JMX.java
+++ aop-mc-int/src/main/org/jboss/aop/microcontainer/aspects/jmx/JMX.java
@@ -38,7 +38,6 @@ import org.jboss.metadata.spi.annotation.InstanceAnnotation;
  */
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.TYPE)
-@InstanceAnnotation(false)
 public @interface JMX 
 {
    /**
