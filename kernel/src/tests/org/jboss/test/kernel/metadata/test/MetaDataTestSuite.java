264da12f9e237aa433764d2f6bf881f6e1390abc
alesj <alesj@84be2c1e-ba19-0410-b317-a758671a6fc1>
2008-08-11 13:06:57 +0000

    Caching metadata repository test.
    git-svn-id: http://anonsvn.jboss.org/repos/jbossas/projects/microcontainer/trunk@76913 84be2c1e-ba19-0410-b317-a758671a6fc1

--- kernel/src/tests/org/jboss/test/kernel/metadata/test/MetaDataTestSuite.java
+++ kernel/src/tests/org/jboss/test/kernel/metadata/test/MetaDataTestSuite.java
@@ -43,6 +43,7 @@ public class MetaDataTestSuite extends TestSuite
       TestSuite suite = new TestSuite("MetaData Tests");
 
       suite.addTest(ClassAnnotationTestCase.suite());
+      suite.addTest(CachingMetaDataRepositoryTestCase.suite());
 
       return suite;
    }
